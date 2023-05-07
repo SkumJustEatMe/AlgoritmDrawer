c01c05e90b96b200720c71a85b5324ef20f2520b
SkumJustEatMe <s214662@dtu.dk>
2023-05-07 16:27:37 +0200

    I can now draw squares black

---
+++ cmake-build-debug/CMakeFiles/MANAlgo.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MANAlgo.dir/main.cpp.obj"
+  "CMakeFiles/MANAlgo.dir/main.cpp.obj.d"
+  "MANAlgo.exe"
+  "MANAlgo.exe.manifest"
+  "MANAlgo.pdb"
+  "libMANAlgo.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MANAlgo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -1,26 +1,40 @@
-
-
+#include <iostream>
 #include <SFML/Graphics.hpp>
 
 const int ROWS = 50;
 const int COLUMS = 50;
 
+void initGrid(sf::RenderWindow& window, sf::RectangleShape grid[][COLUMS]);
 void drawGrid(sf::RenderWindow& window, sf::RectangleShape grid[][COLUMS]);
-void drawTiles(sf::RenderWindow& window, sf::RectangleShape grid[][COLUMS]);
+void drawTiles(sf::RenderWindow& window, sf::RectangleShape tile);
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(800, 800), "Algo");
     sf::RectangleShape grid[ROWS][COLUMS];
 
+    initGrid(window, grid);
+
     while (window.isOpen())
     {
         sf::Event event;
         while (window.pollEvent(event))
         {
+
             if (event.type == sf::Event::Closed)
             {
                 window.close();
+            }
+            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape){
+                window.close();
+            }
+            if (event.type == sf::Event::MouseButtonPressed){
+                if ( event.mouseButton.button == sf::Mouse::Right){
+                    sf::Vector2i mousePosition = sf::Mouse::getPosition(window);
+                    grid[mousePosition.x/16][mousePosition.y/16].setFillColor(sf::Color::Black);
+                    std::cout << mousePosition.x/15 << "\t" << mousePosition.y/15 << "\n";
+
+                }
             }
         }
 
@@ -33,6 +47,16 @@ int main()
 }
 
 void drawGrid(sf::RenderWindow& window, sf::RectangleShape grid[][COLUMS]){
+    for (int i = 0; i < ROWS; i++)
+    {
+        for (int j = 0; j < COLUMS; j++)
+        {
+            window.draw(grid[i][j]);
+        }
+    }
+}
+
+void initGrid(sf::RenderWindow& window, sf::RectangleShape grid[][COLUMS]){
 
     for (int i = 0; i < ROWS; i++)
     {
@@ -43,17 +67,9 @@ void drawGrid(sf::RenderWindow& window, sf::RectangleShape grid[][COLUMS]){
             grid[i][j].setFillColor(sf::Color::White);
         }
     }
-
-    for (int i = 0; i < ROWS; i++)
-    {
-        for (int j = 0; j < COLUMS; j++)
-        {
-            window.draw(grid[i][j]);
-        }
-    }
 }
 
-void drawTiles(sf::RenderWindow& window, sf::RectangleShape grid[][COLUMS]){
-    
+void drawTiles(sf::RenderWindow& window, sf::RectangleShape tile){
+    tile.setFillColor(sf::Color::Black);
 }
 
